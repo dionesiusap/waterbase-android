17f435ed8fe52d8c8ced9ef979ba021abaa86bff
Dionesius Agung <dionesiusap@gmail.com>
2019-02-25 22:49:40 +0700

    implemented http request to turn appliances on/off when user moves upstairs on downstairs

--- app/src/main/java/com/example/waterbase/pbd/BarometerReaderService.java
+++ app/src/main/java/com/example/waterbase/pbd/BarometerReaderService.java
@@ -9,7 +9,13 @@ import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 public class BarometerReaderService extends Service implements SensorEventListener {
     private static final String DEBUG_TAG = "BaroLoggerService";
@@ -18,6 +24,9 @@ public class BarometerReaderService extends Service implements SensorEventListen
     private Sensor sensor = null;
 
     private static float sensorValue = (float) 0.0;
+
+    private int currentFloor = 0;
+    private int previousFloor = 0;
 
     public static void setSensorValue(float value) {
         sensorValue = value;
@@ -60,11 +69,44 @@ public class BarometerReaderService extends Service implements SensorEventListen
         protected Void doInBackground(SensorEvent... events) {
             SensorEvent event = events[0];
             float value = event.values[0];
-            // TODO: send request to turn lights on or off when certain value is reached
             Log.d("BaroLoggerService", "Baro = " + Float.toString(value));
             BarometerReaderService.setSensorValue(value);
 
+            previousFloor = currentFloor;
+            currentFloor = getFloorLevel();
+
+            if (isFloorChanged()) {
+                onFloorChanged();
+            }
+
             return (null);
         }
+    }
+
+    public int getFloorLevel() {
+        return (1 + (int)((FragmentActivity.getGroundFloorBaseline() - getSensorValue()) / 0.25));
+    }
+
+    public boolean isFloorChanged() {
+        return (currentFloor != previousFloor);
+    }
+
+    public void onFloorChanged() {
+        String url = FragmentActivity.getServerUrl() + "/floor/" + Integer.toString(currentFloor);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+                    //Success callback
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    //Failure Callback
+                }
+            }
+        );
+        HttpRequestApplication.getInstance().addToRequestQueue(jsonObjReq, "OnOffRequest");
     }
 }
