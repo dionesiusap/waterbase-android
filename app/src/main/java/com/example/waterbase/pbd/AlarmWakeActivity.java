1064d3fa25d496115bf6a8a1c4e90a11d04a50ab
aditbro <adityafarizki@live.com>
2019-02-25 21:50:03 +0700

    added sensorlistener for shake

--- app/src/main/java/com/example/waterbase/pbd/AlarmWakeActivity.java
+++ app/src/main/java/com/example/waterbase/pbd/AlarmWakeActivity.java
@@ -1,20 +1,41 @@
 package com.example.waterbase.pbd;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-public class AlarmWakeActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AlarmWakeActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_wake);
 
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TextView timeView = findViewById(R.id.wake_time);
+        timeView.setText(Integer.toString(hour) + ":" + Integer.toString(minute));
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
 
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
