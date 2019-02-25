61ea14b9bf6d49e46790c5d85ce73d95def60550
aditbro <adityafarizki@live.com>
2019-02-25 21:37:20 +0700

    alarm is now persistent

--- app/src/main/java/com/example/waterbase/pbd/Alarm.java
+++ app/src/main/java/com/example/waterbase/pbd/Alarm.java
@@ -3,7 +3,9 @@ package com.example.waterbase.pbd;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -22,7 +24,9 @@ import android.widget.ToggleButton;
 import org.w3c.dom.Text;
 
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 import static android.content.Context.ALARM_SERVICE;
 
@@ -31,7 +35,7 @@ public class Alarm extends Fragment {
     AlarmManager alarmManager;
     PendingIntent alarmIntent;
 
-    private PendingIntent pendingIntent;
+    SharedPreferences alarmPreference;
     private TimePicker alarmTimePicker;
     final LinkedList<View> alarmEntry = new LinkedList<>();
     private Alarm inst;
@@ -67,17 +71,23 @@ public class Alarm extends Fragment {
                 addNewAlarm(v);
             }
         });
+
+        alarmPreference = getContext().getSharedPreferences("alarmPref", Context.MODE_PRIVATE);
+        Set<String> alarmIdSet = alarmPreference.getStringSet("alarmList", new HashSet<String>());
+
+        if(!alarmIdSet.isEmpty()){
+            for(String id : alarmIdSet) {
+                appendNewAlarmView(getOldAlarmView((Integer.parseInt(id))));
+            }
+        }
     }
 
     public void addNewAlarm(View v) {
         Calendar mCurrentTime = Calendar.getInstance();
 
-        LayoutInflater inflater =  LayoutInflater.from(getContext());
-        final View newAlarmEntry = inflater.inflate(R.layout.alarm_card, null, false);
-        newAlarmEntry.setId(alarmEntry.size());
-        alarmEntry.add(newAlarmEntry);
-        LinearLayout alarmView = (LinearLayout) getView().findViewById(R.id.alarm_main_layout);
-        alarmView.addView(newAlarmEntry);
+        final int alarmId = alarmEntry.size();
+        final View newAlarmEntry = getNewAlarmView(alarmId);
+        appendNewAlarmView(newAlarmEntry);
 
         int hour = mCurrentTime.get(Calendar.HOUR_OF_DAY);
         int minute = mCurrentTime.get(Calendar.MINUTE);
@@ -86,13 +96,36 @@ public class Alarm extends Fragment {
             public void onTimeSet(TimePicker view, int hour, int minute) {
                 TextView timeView = newAlarmEntry.findViewById(R.id.text_time);
                 timeView.setText(Integer.toString(hour) + ":" + Integer.toString(minute));
-                addNewAlarmService(hour, minute);
+                addNewAlarmService(hour, minute, alarmId);
             }
         }, hour, minute, true);
         mTimePicker.show();
     }
 
-    public void addNewAlarmService(int hour, int minute) {
+    public View getNewAlarmView(int id) {
+        LayoutInflater inflater =  LayoutInflater.from(getContext());
+        final View newAlarmEntry = inflater.inflate(R.layout.alarm_card, null, false);
+        newAlarmEntry.setId(id);
+        alarmEntry.add(newAlarmEntry);
+
+        return newAlarmEntry;
+    }
+
+    public View getOldAlarmView(int id) {
+        View alarmView = getNewAlarmView(id);
+        TextView alarmTime = alarmView.findViewById(R.id.text_time);
+        String alarmString = alarmPreference.getString(Integer.toString(id), null);
+        alarmTime.setText(alarmString);
+
+        return alarmView;
+    }
+
+    public void appendNewAlarmView(View newAlarmEntry) {
+        LinearLayout alarmView = getView().findViewById(R.id.alarm_main_layout);
+        alarmView.addView(newAlarmEntry);
+    }
+
+    public void addNewAlarmService(int hour, int minute, int id) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.set(Calendar.HOUR_OF_DAY, hour);
@@ -101,11 +134,28 @@ public class Alarm extends Fragment {
         Intent intent = new Intent(getContext(), AlarmReceiver.class);
         alarmIntent = PendingIntent.getBroadcast(getContext(), 0, intent, 0);
 
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                1000 * 60 * 20, alarmIntent);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
     }
 
     public void setAlarmText(String alarmText) {
         alarmTextView.setText(alarmText);
+    }
+
+    @Override
+    public void onDestroyView () {
+        super.onDestroyView();
+
+        SharedPreferences.Editor alarmEditor = alarmPreference.edit();
+        Set<String> alarmIdSet = new HashSet<>();
+        Set<String> alarmStringSet = new HashSet<>();
+
+        for(View alarmView : alarmEntry) {
+            alarmIdSet.add(Integer.toString(alarmView.getId()));
+            TextView alarmTime = alarmView.findViewById(R.id.text_time);
+            alarmEditor.putString(Integer.toString(alarmView.getId()), alarmTime.getText().toString());
+        }
+
+        alarmEditor.putStringSet("alarmList", alarmIdSet);
+        alarmEditor.commit();
     }
 }
